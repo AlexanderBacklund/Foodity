fba4bf210d5dbc5c13a8674f2579ad361014ae74
Jakob <h@lldin.se>
2019-02-10 21:18:44 +0100

    cards

--- FoodDonationApp/App.js
+++ FoodDonationApp/App.js
@@ -1,12 +1,22 @@
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, Button} from 'react-native';
+import {Platform, StyleSheet, Text, View, Button, ScrollView, Animated, Image, Dimensions} from 'react-native';
 import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
 import Geolocation from 'react-native-geolocation-service';
 import {PermissionsAndroid} from 'react-native';
 
-  
+console.disableYellowBox = true;
 
+const images = [
+  {uri: "https://icase.azureedge.net/imagevaultfiles/id_124661/cf_259/blomkalsris-med-solrosfron-719531-liten.jpg"},
+  {uri: "https://icase.azureedge.net/imagevaultfiles/id_131896/cf_259/amaranth-romsas-720001-liten.jpg"},
+  {uri: "https://icase.azureedge.net/imagevaultfiles/id_109160/cf_259/kramig-soppa-med-broccoli-palsternacka-och-adelost.png"},
+  {uri: "https://icase.azureedge.net/imagevaultfiles/id_86557/cf_259/jultallrik-med-sill-717057.png"}
+]
 
+const {width, height} = Dimensions.get("window");
+
+const cardHeight = height / 4;
+const cardWidth = cardHeight - 50;
 
 type Props = {};
 class App extends Component<Props> {
@@ -18,27 +28,70 @@ class App extends Component<Props> {
       latitudeDelta: 0.015,
       longitudeDelta: 0.0121,
     },
-    locationChosen: false
-  }
-  
-  pickLocationHandler = event => {
-    const coords = event.nativeEvent.coordinate;
-    this.map.animateToRegion({
-      ...this.state.focusLocation,
-      latitude: coords.latitude,
-      longitude: coords.longitude
-    });
-    this.setState(prevState => {
-      return {
-        focusLocation: {
-          ...prevState.focusLocation,
-          latitude: coords.latitude,
-          longitude: coords.longitude
+    locationChosen: false,
+    markers: [
+      {
+        coordinate: {
+        latitude: 59.334591,
+        longitude: 18.053240,
         },
-        locationChosen: true
-      };
-    });
-  }
+        title: "Blomkal",
+        description: "gul och god",
+        image: images[0],
+      },
+      {
+        coordinate: {
+        latitude: 59.344591,
+        longitude: 18.073240,
+      },
+      title: "Romsas",
+      description: "God till Lax",
+      image: images[1],
+    },{
+      coordinate: {
+        latitude: 59.324591,
+        longitude: 18.083240,
+      },
+      title: "3",
+      description: "också bra",
+      image: images[2],
+    },
+    {
+      coordinate: {
+        latitude: 59.354591,
+        longitude: 18.063240,
+      },
+      title: "4",
+      description: "mums",
+      image: images[3],
+    },
+    ],
+    region: {
+      latitude: 45.52220671242907,
+      longitude: -122.6653281029795,
+      latitudeDelta: 0.04864195044303443,
+      longitudeDelta: 0.040142817690068,
+    },
+  };
+  
+  // pickLocationHandler = event => {
+  //   const coords = event.nativeEvent.coordinate;
+  //   this.map.animateToRegion({
+  //     ...this.state.focusLocation,
+  //     latitude: coords.latitude,
+  //     longitude: coords.longitude
+  //   });
+  //   this.setState(prevState => {
+  //     return {
+  //       focusLocation: {
+  //         ...prevState.focusLocation,
+  //         latitude: coords.latitude,
+  //         longitude: coords.longitude
+  //       },
+  //       locationChosen: true
+  //     };
+  //   });
+  // }
 
 
   getLocationHandler = () => {
@@ -59,6 +112,45 @@ class App extends Component<Props> {
   })
   }
 
+//   updateRestaurantPosition(restaurantPosition){
+//     this.setState({restaurantPosition})
+//   const restaurant = firebase.database().ref('restaurant/' + this.restaurantId)
+//   restaurant.update({restaurantPosition})
+// }
+
+componentWillMount() {
+  this.index = 0;
+  this.animation = new Animated.Value(0);
+}
+componentDidMount() {
+  // We should detect when scrolling has stopped then animate
+  // We should just debounce the event listener here
+  this.animation.addListener(({ value }) => {
+    let index = Math.floor(value / cardWidth + 0.3); // animate 30% away from landing on the next item
+    if (index >= this.state.markers.length) {
+      index = this.state.markers.length - 1;
+    }
+    if (index <= 0) {
+      index = 0;
+    }
+
+    clearTimeout(this.regionTimeout);
+    this.regionTimeout = setTimeout(() => {
+      if (this.index !== index) {
+        this.index = index;
+        const { coordinate } = this.state.markers[index];
+        this.map.animateToRegion(
+          {
+            ...coordinate,
+            latitudeDelta: this.state.region.latitudeDelta,
+            longitudeDelta: this.state.region.longitudeDelta,
+          },
+          350
+        );
+      }
+    }, 10);
+  });
+}
 
   render() {
     let marker = null;
@@ -66,6 +158,25 @@ class App extends Component<Props> {
     if (this.state.locationChosen) {
       marker = <MapView.Marker coordinate={this.state.focusLocation} />
     }
+
+    const interpolations = this.state.markers.map((marker, index) => {
+      const inputRange = [
+        (index - 1) * cardWidth,
+        index * cardWidth,
+        ((index + 1) * cardWidth),
+      ];
+      const scale = this.animation.interpolate({
+        inputRange,
+        outputRange: [1, 2.5, 1],
+        extrapolate: "clamp",
+      }); 
+      const opacity = this.animation.interpolate({
+        inputRange,
+        outputRange: [0.35, 1, 0.35],
+        extrapolate: "clamp",
+      });
+      return { scale, opacity };
+    });
     return (
       <View
       style={styles.container}>
@@ -79,16 +190,68 @@ class App extends Component<Props> {
        ref={ref => this.map = ref}
       >
       {marker}
-     <MapView.Marker 
+      {this.state.markers.map((marker, index) => {
+        return (
+          <MapView.Marker key={index} coordinate={marker.coordinate}>
+          <Animated.View style={styles.markerWrap}>
+            <View >
+            <Image source={require('./src/images/FoodityIcon2.png')} style={{width: 50, height: 50}}/>
+            </View>
+          </Animated.View>
+
+          </MapView.Marker>
+        )
+      })}
+     {/* <MapView.Marker 
             coordinate={{latitude: 59.334591, longitude: 18.063240}}
             title={"title"}
             description={"description"}
-         />
+         /> */}
          
      </MapView>
+
+     <Animated.ScrollView
+          horizontal
+          scrollEventThrottle={1}
+          showsHorizontalScrollIndicator={false}
+          snapToInterval={cardWidth}
+          onScroll={Animated.event(
+            [
+              {
+                nativeEvent: {
+                  contentOffset: {
+                    x: this.animation,
+                  },
+                },
+              },
+            ],
+            { useNativeDriver: true }
+          )}
+          style={styles.scrollView}
+          contentContainerStyle={styles.endPadding}
+        >
+          {this.state.markers.map((marker, index) => (
+            <View style={styles.card} key={index}>
+              <Image
+                source={marker.image}
+                style={styles.cardImage}
+                resizeMode="cover"
+              />
+              <View style={styles.textContent}>
+                <Text numberOfLines={1} style={styles.cardtitle}>{marker.title}</Text>
+                <Text numberOfLines={1} style={styles.cardDescription}>
+                  {marker.description}
+                </Text>
+              </View>
+            </View>
+          ))}
+        </Animated.ScrollView>
+
      <View style={styles.locateButton}> 
      <Button 
-     title="Locate me" onPress={this.getLocationHandler}/>
+     title="Locate me" onPress={this.getLocationHandler}
+     color="#5eb56a"
+     />
      </View>
      
 
@@ -119,8 +282,59 @@ const styles = StyleSheet.create({
     bottom: 0,
     right: 0,
   },
+  markerWrap: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  marker: {
+    width: 24,
+    height: 24,
+    borderRadius: 24,
+    backgroundColor: "#5eb56a",
+  },
   locateButton: {
-    bottom: 50
+    bottom: 600,
+  },
+  scrollView: {
+    position: "absolute",
+    bottom: 30,
+    left: 0,
+    right: 0,
+    paddingVertical: 10,
+  },
+  endPadding: {
+    paddingRight: width - cardWidth,
+  },
+  card: {
+    padding: 10,
+    elevation: 2,
+    backgroundColor: "#FFF",
+    marginHorizontal: 10,
+    shadowColor: "#000",
+    shadowRadius: 5,
+    shadowOpacity: 0.3,
+    shadowOffset: { x: 2, y: -2 },
+    height: cardHeight,
+    width: cardWidth,
+    overflow: "hidden",
+  },
+  cardImage: {
+    flex: 3,
+    width: "100%",
+    height: "100%",
+    alignSelf: "center",
+  },
+  textContent: {
+    flex: 1,
+  },
+  cardtitle: {
+    fontSize: 12,
+    marginTop: 5,
+    fontWeight: "bold",
+  },
+  cardDescription: {
+    fontSize: 12,
+    color: "#444",
   }
 });
 
