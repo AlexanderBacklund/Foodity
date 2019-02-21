dabf4e30844b8dc972ba67ae73e5904505a31aed
Hamza <hamza.imran@attech-ltd.com>
2019-02-21 17:35:57 +0100

    new fields and verification added

--- FoodDonationApp/src/screens/Login.js
+++ FoodDonationApp/src/screens/Login.js
@@ -3,28 +3,34 @@ import {StyleSheet,Text,View,TextInput,Button,TouchableHighlight,Image,Alert,Key
 import firebase from 'firebase';
 import SignUp from './SignUp';
 import Loading from './Loading';
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCDNg-6wLAG9uO695FAyMlvWlnjWEBsY50",
-//   authDomain: "food-donation-bcce1.firebaseapp.com",
-//   databaseURL: "https://food-donation-bcce1.firebaseio.com",
-//   projectId: "food-donation-bcce1",
-//   storageBucket: "food-donation-bcce1.appspot.com",
-//   messagingSenderId: "474995894111",
-// };
+import Browse from './Browse';
+// import Icon from 'react-native-vector-icons/AntDesign';
+import { Input } from 'react-native-elements';
 
-// const app = firebase.initializeApp(firebaseConfig);
 
 export default class Login extends React.Component {
 
   state = { email: '', password: '', errorMessage: null }
-
   loginHandler = () => {
     const { email, password } = this.state
     firebase
       .auth()
       .signInWithEmailAndPassword(email, password)
-      .then(() =>{ Alert.alert('Logged In')
-      this.props.navigation.navigate('Loading')
+      .then(() =>{ var user = firebase.auth().currentUser; 
+        // If email verification is commented out in SignUp.js, then if condition should be (user.emailVerified==true) else it should be (user.emailVerified==false). Just for testing.
+        if (user.emailVerified==false) {
+          this.setState({ errorMessage: "Email not verified. Please click in the verification link sent to your email address." })
+        } else {
+          this.props.navigation.navigate('Browse')
+        }
+        var db = firebase.database().ref('UsersList/'+user.uid);
+        db.once('value').then( function (snap) {
+          console.log((snap.val().typeOfUser)=="Restaurant"); // Check type of user
+          console.log(user.emailVerified); 
+         });
+        // console.log(db.email);
+      //   this.props.navigation.navigate('LocationGeo')
+      // this.props.navigation.navigate('Browse')
       }).catch(error => this.setState({ errorMessage: error.message }))
     console.log('loginHandler')
   }
@@ -37,6 +43,8 @@ export default class Login extends React.Component {
         <View style={styles.inputContainer}>
           <TextInput style={styles.inputs}
               placeholder="Email"
+              // leftIcon={{ type:'antdesign', name: 'mail' ,color: 'grey'}}
+              // leftIconContainerStyle={{marginLeft:0, marginBottom: 3, marginRight: 5}}
               keyboardType="email-address"
               underlineColorAndroid='transparent'
               onChangeText={(email) => this.setState({email})}/>
@@ -45,6 +53,10 @@ export default class Login extends React.Component {
         <View style={styles.inputContainer}>
           <TextInput style={styles.inputs}
               placeholder="Password"
+              // leftIcon={{ type:'antdesign', name: 'lock' ,color: 'grey'}}
+              // leftIconContainerStyle={{marginLeft:0, marginBottom: 3, marginRight: 5}}
+              // rightIcon={{type:'antdesign', name: 'eye' ,color: 'grey'}}
+              // rightIconContainerStyle={{marginLeft:0, marginBottom: 3, marginRight: 5}}
               secureTextEntry={true}
               underlineColorAndroid='transparent'
               onChangeText={(password) => this.setState({password})}/>
@@ -58,10 +70,19 @@ export default class Login extends React.Component {
             {this.state.errorMessage}
           </Text>}
           {/* {console.log(this.state.errorMessage)} */}
-        
-        <TouchableHighlight style={[styles.buttonContainer, styles.registerButton]} onPress={() => this.props.navigation.navigate('SignUp')}>
-            <Text>Not a member? Sign up now.</Text>
+
+          <View>
+          <Text style={{ color: 'black', marginLeft: 40, marginRight: 20, marginBottom: 20 }}>Not Registered? Don't worry. You can Sign Up below.</Text>
+        </View>
+
+          <View style={styles.containerTwo}>
+        <TouchableHighlight style={[styles.registerButton]} onPress={() => this.props.navigation.navigate('SignUp', {text: 'Restaurant' })}>
+            <Text>Restaurant</Text>
         </TouchableHighlight>
+        <TouchableHighlight style={[styles.registerButton]} onPress={() => this.props.navigation.navigate('SignUp', {text: 'Charity' })}>
+            <Text>Charity</Text>
+        </TouchableHighlight>
+        </View>
       </KeyboardAvoidingView>
     );
   }
@@ -75,10 +96,23 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#5eb56a',
   },
+  containerTwo: {
+    // flex: 1,
+    // flexDirection: 'row',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom:20,
+    height: 45,
+    // width:250,
+    borderRadius:30,
+    backgroundColor: '#5eb56a',
+    
+  },
   inputContainer: {
       borderBottomColor: '#4076ce',
       backgroundColor: '#FFFFFF',
-      borderRadius:30,
+      borderRadius:10,
       borderBottomWidth: 1,
       width:250,
       height:45,
@@ -95,7 +129,7 @@ const styles = StyleSheet.create({
   inputIcon:{
     width:30,
     height:30,
-    marginLeft:15,
+    // marginLeft:15,
     justifyContent: 'center'
   },
   buttonContainer: {
@@ -109,6 +143,12 @@ const styles = StyleSheet.create({
   },
   registerButton: {
     backgroundColor: 'rgba(13, 65, 168,0.4)',
+    width:'30%',
+    height:40,
+    borderRadius:30,
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin:15,
   },
   loginButton: {
     backgroundColor: "#00b5ec",
